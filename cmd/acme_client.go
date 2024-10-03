8450a910b26cabadc04861765b80fe6184ea1523
Adam Krivka <akrivka@caltech.edu>
2024-10-03 10:38:07 +0200

    Refactor main function into multiple smaller functions

--- cmd/acme_client.go
+++ cmd/acme_client.go
@@ -5,6 +5,8 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
@@ -28,7 +30,14 @@ type ACMEDirectory struct {
 
 type ACMEServer struct {
 	Directory ACMEDirectory
+	NextNonce string
 }
+
+// Our ACME Server (currently we do not support trying multiple servers)
+var server ACMEServer
+
+// ACME server's CA certificate (global because used by multiple HTTP clients)
+var serverCACertPool *x509.CertPool
 
 // Types of challenges
 const (
@@ -36,53 +45,53 @@ const (
 	ChallengeDNS01  = "dns01"
 )
 
-// Type of challenge we'll be using to prove ownership of domain
-var challengeType string
-
-// Our ACME Server (currently we do not support trying multiple servers)
-var server ACMEServer
-
-// Next nonce to use from last server response
-var nextNonce string
-
 // Command-line arguments configuration for go-flags
 var opts struct {
-	Directory   string   `long:"dir" description:"Directory URL of the ACME Server" required:"true"`
-	IPV4Address string   `long:"record" description:"IPv4 address to be returned by the DNS server" required:"true"`
-	Domains     []string `long:"domain" description:"Domain for which to request certificate. Can have multiple --domain flags for multiple domains." required:"true"`
-	Revoke      bool     `long:"revoke" description:"Should we revoke certificate after obtaining it"`
-	CACert      string   `long:"cert" description:"Path to the CA certificate of the ACME server (added for local debugging with Pebble)" default:"project/pebble.minica.pem"`
+	ChallengeType string
+	Directory     string   `long:"dir" description:"Directory URL of the ACME Server" required:"true"`
+	IPV4Address   string   `long:"record" description:"IPv4 address to be returned by the DNS server" required:"true"`
+	Domains       []string `long:"domain" description:"Domain for which to request certificate. Can have multiple --domain flags for multiple domains." required:"true"`
+	Revoke        bool     `long:"revoke" description:"Should we revoke certificate after obtaining it"`
+	CACert        string   `long:"cert" description:"Path to the CA certificate of the ACME server (added for local debugging with Pebble)" default:"project/pebble.minica.pem"`
 }
 
-func main() {
-	slog.Info("Starting the ACME client")
-
-	// Check validity of the challenge type
-	challengeType = os.Args[1]
-	if !(challengeType == ChallengeHTTP01 || challengeType == ChallengeDNS01) {
-		slog.Error("Invalid challenge type (http01 | dns01)")
-		os.Exit(1)
+// Parse command line arguments and store them in `opts` and `challengeType`
+func parseCmdArgs() error {
+	if len(os.Args) < 2 {
+		return errors.New("missing challenge type argument")
 	}
 
-	// Parse the remaining arguments
-	_, err := flags.ParseArgs(&opts, os.Args[2:])
-	if err != nil {
-		slog.Error("Failed to parse command-line arguments", "message", err.Error())
-		os.Exit(1)
+	opts.ChallengeType = os.Args[1]
+	if !(opts.ChallengeType == ChallengeHTTP01 || opts.ChallengeType == ChallengeDNS01) {
+		return errors.New("invalid challenge type (http01 | dns01)")
 	}
 
-	// Install ACME server's public certificate
+	if _, err := flags.ParseArgs(&opts, os.Args[2:]); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Install ACME server's public certificate and initialize the HTTP client
+func readCACert() error {
 	caCert, err := os.ReadFile(opts.CACert)
 	if err != nil {
-		slog.Error("Failed to read ACME server's CA certificate", "error", err)
+		return err
 	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
+	serverCACertPool = x509.NewCertPool()
+	serverCACertPool.AppendCertsFromPEM(caCert)
+	return nil
+}
 
+// Initialize global variable `server` with the server's directory URLs
+// and fetch the first replay nonce
+func initACMEServer() error {
+	// Initialize a HTTP client with only the TLS middleware
+	// for this initial exchange
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
+				RootCAs: serverCACertPool,
 			},
 		},
 	}
@@ -90,29 +99,51 @@ func main() {
 	// Fetch the directory endpoint and save the urls
 	res, err := client.Get(opts.Directory)
 	if err != nil {
-		slog.Error("Failed to fetch the ACME server directory", "error", err)
-		os.Exit(1)
+		return fmt.Errorf("directory HTTP call failed (%s)", err.Error())
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("directory HTTP call unexpected status code (%d)", res.StatusCode)
 	}
 
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
-		slog.Error("Failed to read ACME server directory", "error", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to read directory response body (%s)", err.Error())
 	}
 
 	err = json.Unmarshal(resBody, &server.Directory)
 	if err != nil {
-		slog.Error("Failed to parse ACME server directory", "error", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to parse directory response body (%s)", err)
 	}
 
 	// Request our first nonce and save it
 	res, err = client.Head(server.Directory.NewNonce)
 	if err != nil {
-		slog.Error("Failed to get first nonce", "error", err)
+		return fmt.Errorf("failed to get first nonce (%s)", err.Error())
+	}
+	server.NextNonce = res.Header["Replay-Nonce"][0]
+
+	return nil
+}
+
+func main() {
+	slog.Info("Starting the ACME client")
+
+	if err := parseCmdArgs(); err != nil {
+		slog.Error("Failed to parse cmd arguments", "error", err.Error())
 		os.Exit(1)
 	}
-	nextNonce = res.Header["Replay-Nonce"][0]
+
+	if err := readCACert(); err != nil {
+		slog.Error("Failed to read server CA certs", "error", err.Error())
+		os.Exit(1)
+	}
+
+	if err := initACMEServer(); err != nil {
+		slog.Error("Failed to read/initialize ACME server", "error", err.Error())
+		os.Exit(1)
+	}
 
 	// Start both challenge servers (doesn't really matter that we only use one)
 	go http01.HTTP01()
